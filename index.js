4f218103c64e1d73666e28b858d2cf4bf5f82fdb
KaladaJumbo <wkalada1@gmail.com>
2020-10-11 18:23:54 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+function submitData(name, email) {
+    let meta = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    }
+    return fetch(`http://localhost:3000/users`, meta)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.body.innerHTML = `${data.id}`
+        })
+        .catch(
+            message => {
+                document.body.innerHTML = `${message}`
+            }
+        )
+}
+
+// const url = "http://localhost:3000/dogs"
+// const meta = {
+//     method: "post"
+//     headers: {
+//         "Content-Type": "application/"
+//     }
+// }
+// document.addEventListener("DOMContentLoaded"() => {
+//     fetching();
+// })
+
+// function fetching(urlLink, meta= {}) {
+//     fetch(urlLink, meta)
+// }
